27d10a83264f22e7221f1364cc502e8d026c2e67
Damilola Oduronbi <doduronbi@gmail.com>
2020-09-10 14:38:18 +0100

    Update solution to use times loop iteration index

--- method1.rb
+++ method1.rb
@@ -4,28 +4,25 @@ puts 'What number do you want this fizzbuzz app to stop?'
 # Store the number in user_input variable
 user_input = gets.chomp.to_i
 
-# Use a counter to loop through and initialize it with 0
-counter = 0
-
 # Start the loop
-user_input.times do
-  # increment the counter
-  counter += 1
+user_input.times do |i|
+  # increment the i
+  i += 1
 
-  # check to see if the counter is divisible by both 3 and 5 and print 'FizzBuzz'
-  if (counter % 3).zero? and (counter % 5).zero?
+  # check to see if the i is divisible by both 3 and 5 and print 'FizzBuzz'
+  if (i % 3).zero? and (i % 5).zero?
     p 'FizzBuzz'
 
-  # check if the counter is divisible by only 3 and print 'Fizz'
-  elsif (counter % 3).zero?
+  # check if the i is divisible by only 3 and print 'Fizz'
+  elsif (i % 3).zero?
     p 'Fizz'
 
-  # check if the counter is divisible by only 5 and print 'Buzz'
-  elsif (counter % 5).zero?
+  # check if the i is divisible by only 5 and print 'Buzz'
+  elsif (i % 5).zero?
     p 'Buzz'
 
-  # if counter doesnt meet any of the conditions above, print the counter
+  # if i doesnt meet any of the conditions above, print the i
   else
-    p counter
+    p i
   end
 end
